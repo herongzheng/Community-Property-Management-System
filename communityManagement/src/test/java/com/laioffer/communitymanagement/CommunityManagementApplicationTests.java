68db6445898a7b7c5d5566ed5693d14e0a062dc4
Roger <rongzheng.he@gmail.com>
2023-08-20 15:28:20 -0400

    initialize backend project

---
+++ communityManagement/src/test/java/com/laioffer/communitymanagement/CommunityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.communitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
