bae86c2ee9f8424b234834d6d780c8aca5e20cd5
Lan <lan.peng234@gmail.com>
2023-08-28 16:27:34 -0700

    Create SecurityConfig

---
+++ communityManagement/src/main/java/com/laioffer/communitymanagement/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.laioffer.communitymanagement.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/issues/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/issues/*").hasAuthority("ROLE_GUEST")
+                .anyRequest().authenticated()
+                .and()
+                .csrf()
+                .disable();
+    }
+}
