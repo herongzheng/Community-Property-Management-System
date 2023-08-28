6107b740071023c91a38f48664bf02de0d7597fe
Lan <lan.peng234@gmail.com>
2023-08-28 12:48:31 -0700

    Update IssueImage

--- communityManagement/src/main/java/com/laioffer/communitymanagement/db/entity/IssueImage.java
+++ communityManagement/src/main/java/com/laioffer/communitymanagement/db/entity/IssueImage.java
@@ -18,11 +18,37 @@ public class IssueImage {
 
     @ManyToOne
     @JoinColumn(name = "issue_id")
-    @JsonIgnore
+//    @JsonIgnore     //????
     private Issue issue;
 
-    //public constructor
-    //private constructor with builder
-    //getter and setter
-    //class Builder
+    public IssueImage() {
+    }
+
+
+    public IssueImage(String url, Issue issue) {
+        this.url = url;
+        this.issue = issue;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+
+    public IssueImage setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+
+    public Issue getStay() {
+        return issue;
+    }
+
+
+    public IssueImage setStay(Issue issue) {
+        this.issue = issue;
+        return this;
+    }
 }
