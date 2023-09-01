90b5d4f5651dc090da083bc5e9237e5ee417f7ef
Lan <lan.peng234@gmail.com>
2023-09-01 10:00:48 -0700

    Implement image uploading

---
+++ communityManagement/src/main/java/com/laioffer/communitymanagement/exception/AmazonS3UploadException.java
@@ -0,0 +1,8 @@
+package com.laioffer.communitymanagement.exception;
+
+public class AmazonS3UploadException extends RuntimeException {
+    public AmazonS3UploadException(String message) {
+        super(message);
+    }
+}
+
