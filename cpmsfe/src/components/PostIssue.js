f92f26325ad3a7c8318a2382558a4fd01dd5d04e
gitihabu <113068668+gitihabu@users.noreply.github.com>
2023-09-04 15:11:14 -0400

    Update PostIssue.js

--- cpmsfe/src/components/PostIssue.js
+++ cpmsfe/src/components/PostIssue.js
@@ -1,1 +1,19 @@
+const PostIssue = () => {
+  const [description, setDescription] = useState('');
 
+  const handlePostIssue = () => {  };
+
+  return (
+    <div>
+      <h2>Post a New Issue</h2>
+      <textarea
+        placeholder="Describe the issue"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button onClick={handlePostIssue}>Submit</button>
+    </div>
+  );
+};
+
+export default PostIssue;
