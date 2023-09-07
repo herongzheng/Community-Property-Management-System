dddc675115547c89e34e97a94305512438085755
XingzhouTang <127881176+XingzhouTang@users.noreply.github.com>
2023-09-07 18:48:17 -0500

    Create UserRepository.java
    添加两个功能：
    1 findByUsername 用来提取用户名，以便后续比较
    2 updateUserDetails 
    
    @Transactional：这个注解表示方法应该在事务中运行。事务是一系列操作，它们被视为一个单一的工作单元。这意味着如果事务中的任何一个操作失败，所有的操作都将被回滚（撤销），保证数据的一致性。
    
    @Modifying：这个注解表示这是一个修改数据库内容的操作，如INSERT、UPDATE或DELETE。它告诉Spring Data JPA，这个查询不是一个简单的数据查询操作。
    
    @Query：这个注解用于提供自定义的SQL或JPQL查询。在此，我们使用的是JPQL（Java Persistence Query Language）查询。
    UPDATE User u SET u.password = ?2, ... WHERE u.username = ?1
    这是JPQL查询的主体部分。它表示我们要更新User实体，具体来说，我们要根据username更新用户的密码、邮箱、电话号码、名和姓。
    
    ?1, ?2 ：是参数的占位符。它们将会被方法参数按顺序替换。例如，当调用updateUserDetails方法时，方法的第一个参数会替换?1，第二个参数会替换?2，依此类推。
    
    当调用这个updateUserDetails方法并传递适当的参数时，它将更新匹配给定username的用户的相关详细信息。

---
+++ communityManagement/src/main/java/com/laioffer/communitymanagement/db/UserRepository.java
@@ -0,0 +1,20 @@
+package com.laioffer.communitymanagement.db;
+
+import com.laioffer.communitymanagement.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.password = ?2, u.email = ?3, u.phoneNumber = ?4, u.firstName = ?5, u.lastName = ?6 WHERE u.username = ?1")
+    void updateUserDetails(String username, String newPassword, String email, String phoneNumber, String firstName, String lastName);
+}
