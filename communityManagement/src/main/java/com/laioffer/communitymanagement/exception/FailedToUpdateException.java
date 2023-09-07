dc0e28da04c207d618bdbb6717ffea4b82514650
XingzhouTang <127881176+XingzhouTang@users.noreply.github.com>
2023-09-07 18:57:44 -0500

    Create FailedToUpdateException.java

---
+++ communityManagement/src/main/java/com/laioffer/communitymanagement/exception/FailedToUpdateException.java
@@ -0,0 +1,7 @@
+package com.laioffer.communitymanagement.exception;
+
+public class FailedToUpdateException extends RuntimeException {
+    public FailedToUpdateException(String message) {
+        super(message);
+    }
+}
