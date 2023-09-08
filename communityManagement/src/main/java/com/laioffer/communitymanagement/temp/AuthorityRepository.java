b11b26e858336af08402c1f5ff3eb31dd17f90dc
XingzhouTang <127881176+XingzhouTang@users.noreply.github.com>
2023-09-07 20:45:27 -0500

    Delete communityManagement/src/main/java/com/laioffer/communitymanagement/temp directory

--- communityManagement/src/main/java/com/laioffer/communitymanagement/temp/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laioffer.communitymanagement.repository;
-
-import com.laioffer.communitymanagement.db.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
--- communityManagement/src/main/java/com/laioffer/communitymanagement/temp/PasswordChangeBody.java
+++
@@ -1,6 +0,0 @@
-package com.laioffer.communitymanagement.model;
-
-public record PasswordChangeBody(String username, String oldPassword, String newPassword,
-                                 String firstName, String lastName, String email,
-                                 String phoneNumber) {
-}
