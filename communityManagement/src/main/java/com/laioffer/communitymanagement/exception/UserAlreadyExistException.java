3a29b5377ea396efd371f59d9ed8938fe5341fc3
XingzhouTang <127881176+XingzhouTang@users.noreply.github.com>
2023-09-07 20:43:55 -0500

    Delete communityManagement/src/main/java/com/laioffer/communitymanagement/exception directory

--- communityManagement/src/main/java/com/laioffer/communitymanagement/exception/UserAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.laioffer.communitymanagement.exception;
-
-public class UserAlreadyExistException extends RuntimeException {
-
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
